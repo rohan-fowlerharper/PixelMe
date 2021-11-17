a28e91f28e065f0b41c119008080c15ae9807bc5
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-11-17 22:23:57 +1300

    note() - add todo

--- pages/from/index.js
+++ pages/from/index.js
@@ -4,6 +4,7 @@ import { nyanCat } from '../../data'
 
 const FromTemplate = () => {
   return (
+    // todo: add fetch data from server and pass as template
     <Editor template={nyanCat} />
   )
 }
