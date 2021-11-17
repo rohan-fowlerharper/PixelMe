8725b322b851cb77e5f1315d34feffbce9ee25b0
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-11-17 22:23:40 +1300

    feat(*) - initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import request from 'superagent'
 
-export default function handler(req, res) {
+export function helloHandler(req, res) {
   res.status(200).json({ name: 'John Doe' })
+}
+
+// TODO: add a GET route for /api/v1/templates/:id
+export function getTemplateHandler(id) {
+  return request
+    .get(`${process.env.API_URL}/templates/${id}`)
+    .then(res => res.body)
+}
+
+// TODO: add a POST route for /api/v1/templates
+export function postTemplateHandler(template) {
+  return request
+    .post(`${process.env.API_URL}/templates`)
+    .send(template)
+    .then(res => res.body)
 }
