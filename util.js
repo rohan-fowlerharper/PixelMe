4e3346e45e9418f8780c028440a62babb9cc6c2f
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-11-18 10:42:18 +1300

    fix() - add Math.floor to luma value to properly render hex

--- util.js
+++ util.js
@@ -1,5 +1,5 @@
 export function hexToGrayscale (hex = "#ffffff") {
   const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16))
-  const averageLuma = (r + g + b) / 3
+  const averageLuma = Math.floor(((r + g + b) / 3))
   return `#${averageLuma.toString(16).padStart(2, '0')}${averageLuma.toString(16).padStart(2, '0')}${averageLuma.toString(16).padStart(2, '0')}`
 }
