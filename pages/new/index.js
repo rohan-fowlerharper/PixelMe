c8500320d8a1b2cfa1129e392e5c3e8f11550c72
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-11-17 22:24:12 +1300

    note() - add todos

--- pages/new/index.js
+++ pages/new/index.js
@@ -5,6 +5,7 @@ import Editor from '../../components/Editor'
 const New = () => {
   return (
     // TODO: allow user to create own color scheme
+    // TODO: allow user to upload own template to server
     <Editor />
   )
 }
