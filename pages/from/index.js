0456907c7fe64ecbe39d753aa68a3a0e371dce9d
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-11-18 10:27:52 +1300

    note() - add todos for navigation

--- pages/from/index.js
+++ pages/from/index.js
@@ -4,6 +4,7 @@ import { nyanCat } from '../../data'
 
 const FromTemplate = () => {
   return (
+    // todo: add breadcrumbs and home button
     // todo: add fetch data from server and pass as template
     <Editor template={nyanCat} />
   )
--- pages/new/index.js
+++ pages/new/index.js
@@ -2,6 +2,7 @@ import Editor from '../../components/Editor'
 
 const New = () => {
   return (
+    // TODO: add breadcrumbs, title and home button
     // TODO: allow user to create own color scheme
     // TODO: allow user to upload own template to server
     <Editor />
