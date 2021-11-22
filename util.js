0da38b1ea4179e570eb36e1e6ee7d5b74c566a17
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-11-22 21:09:15 +1300

    feat() - set greyscale to half luma

--- util.js
+++ util.js
@@ -1,5 +1,8 @@
 export function hexToGrayscale (hex = "#ffffff") {
   const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16))
   const averageLuma = Math.floor(((r + g + b) / 3))
-  return `#${averageLuma.toString(16).padStart(2, '0')}${averageLuma.toString(16).padStart(2, '0')}${averageLuma.toString(16).padStart(2, '0')}`
+  const offsetLuma = Math.floor((255 - averageLuma) * 0.6)
+  const finalLuma = averageLuma + offsetLuma
+  const grayscale = finalLuma.toString(16)
+  return `#${grayscale}${grayscale}${grayscale}`
 }
