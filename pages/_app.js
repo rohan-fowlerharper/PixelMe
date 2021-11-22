cb8bc5aef1004b4da34f40a5340185327cd3381e
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-11-22 23:03:11 +1300

    fix() - revert to system ui font

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
-import "@fontsource/poppins"
 import { 
   ChakraProvider,
   Container
 } from '@chakra-ui/react'
-import theme from '../components/theme'
 import NavBar from '../components/NavBar'
 import { Provider } from 'react-redux'
 import store from '../redux/store'
@@ -20,7 +18,7 @@ import store from '../redux/store'
 function MyApp ({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider>
         <NavBar />
         <Container maxW='6xl' mt='10'>
           <Component {...pageProps} />
