5c8a8569cb56cb6249a5ff88e0f53ba50092f57d
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-11-22 21:13:33 +1300

    feat(SET_COLOR) - change ambiguous action type

--- redux/reducers/selectedColor.js
+++ redux/reducers/selectedColor.js
@@ -2,7 +2,7 @@ const initialState = '#FFFFFF'
 
 function selectedColor (state = initialState, action) {
   switch (action.type) {
-    case 'SET_COLOR':
+    case 'SET_SELECTED_COLOR':
       return action.color
     default:
       return state
