0e301a7b0004decc771e1e539760eb02bf466c73
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2023-05-16 15:07:56 +1200

    fix: hydration issue with next/link

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,22 @@ export default function Home() {
   const colorScheme = useColorScheme()
   return (
     <Flex justify='space-evenly'>
-      <Link href='/new' passHref>
-        <Button as='a' colorScheme={colorScheme} leftIcon={<AddIcon />}>
-          New Drawing
-        </Button>
-      </Link>
-      <Link href='/from-template' passHref>
-        <Button as='a' colorScheme={colorScheme} leftIcon={<StarIcon />}>
-          From A Template
-        </Button>
-      </Link>
+      <Button
+        as={Link}
+        href='/new'
+        colorScheme={colorScheme}
+        leftIcon={<AddIcon />}
+      >
+        New Drawing
+      </Button>
+      <Button
+        as={Link}
+        href='/from-template'
+        colorScheme={colorScheme}
+        leftIcon={<StarIcon />}
+      >
+        From A Template
+      </Button>
     </Flex>
   )
 }
